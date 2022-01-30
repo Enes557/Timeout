790a13f6d37943ef5604a6c3fd71701dd565a39f
Enes <58441855+Enes557@users.noreply.github.com>
2022-01-30 14:40:43 +0300

    Update example.js

--- example.js
+++ example.js
@@ -1,5 +1,5 @@
 const timeout = require('./index.js')
-var token = "OTE4ODI0MDEwMTIzNDYwNjM4.YbM3ig.vGsoBXmlEttYrc3qLRUDJYfaqkw"
-var guild = "906916377204506694"
-var user = "901729076522414082"
-timeout.off(token, guild, user, "10m")
+var token = "token"
+var guild = "sunucu id"
+var user = "mutelencek kişinin id"
+timeout.off(token, guild, user, "süre")
